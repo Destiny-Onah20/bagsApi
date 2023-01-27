c7d20d479e95799715ed9896ccab0468a063899f
Destiny-Onah20 <onahdestinyc20@gmail.com>
2023-01-27 17:58:57 +0100

    making additional changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({path: './config/config.env'})
-const app = require('./App')
+const app = require('./app')
 
 
 const port = process.env.PORT
